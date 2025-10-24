2afe75ee15921195792a97eba1bd9117f0083cc8
Shamsuddeen Omacy <omacys2@gmail.com>
2025-10-25 00:35:40 +0100

    Init

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./src/app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taskmanager';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taska';
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
