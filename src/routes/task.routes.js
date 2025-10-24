e5f3c3b84f032a83bce190a0e45e695e0e7b7818
Shamsuddeen Omacy <omacys2@gmail.com>
2025-10-25 00:33:40 +0100

    App Routes

---
+++ src/routes/task.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/task.controller');
+
+router.post('/', taskController.create);
+router.get('/', taskController.getAll);
+router.get('/stats', taskController.getStats);
+router.get('/:id', taskController.getById);
+router.put('/:id', taskController.update);
+router.delete('/:id', taskController.delete);
+
+module.exports = router;
